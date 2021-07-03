838b690bcf0bf28a9e22d89beda29a0db65209d7
YChaeeun <ychae.leah@gmail.com>
2021-07-04 00:54:04 +0900

    [codelab] basic test - prepare ServiceLoacator for Tests

--- app/src/main/java/com/example/android/architecture/blueprints/todoapp/ServiceLocator.kt
+++ app/src/main/java/com/example/android/architecture/blueprints/todoapp/ServiceLocator.kt
@@ -1,6 +1,7 @@
 package com.example.android.architecture.blueprints.todoapp
 
 import android.content.Context
+import androidx.annotation.VisibleForTesting
 import androidx.room.Room
 import com.example.android.architecture.blueprints.todoapp.data.source.DefaultTasksRepository
 import com.example.android.architecture.blueprints.todoapp.data.source.TasksDataSource
@@ -8,8 +9,14 @@ import com.example.android.architecture.blueprints.todoapp.data.source.TasksRepo
 import com.example.android.architecture.blueprints.todoapp.data.source.local.TasksLocalDataSource
 import com.example.android.architecture.blueprints.todoapp.data.source.local.ToDoDatabase
 import com.example.android.architecture.blueprints.todoapp.data.source.remote.TasksRemoteDataSource
+import kotlinx.coroutines.runBlocking
 
 object ServiceLocator {
+	// One of the downsides of using a service locator is that it is a "shared singleton".
+	// 1. when the test finishes, need to reset the state of service locator
+	// 2. cannot run tests in parallel
+
+	private val lock = Any()
 
 	private var database: ToDoDatabase? = null
 
@@ -17,6 +24,25 @@ object ServiceLocator {
 	// make sure volatile variable is always up-to-date & same to all executed threads
 	@Volatile
 	var tasksRepository: TasksRepository? = null
+		@VisibleForTesting set // way to express that the reason the setter is pubic is because of testing
+
+	@VisibleForTesting
+	fun resetRepository() {
+		synchronized(lock) {
+			runBlocking {
+				TasksRemoteDataSource.deleteAllTasks()
+			}
+
+			// Clear all data to avoid test pollution
+			database?.apply {
+				clearAllTables()
+				close()
+			}
+
+			database = null
+			tasksRepository = null
+		}
+	}
 
 	fun provideTasksRepository(context: Context): TasksRepository {
 		synchronized(this) { // avoid creating more than one repository instance
